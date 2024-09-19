1930bb467d5aafba2bdcf8d97ee012eade26ef43
2382007 <shuimiracle@gmail.com>
2024-09-19 21:43:30 +0700

    Tugas1

--- Soal123/Soal3.java
+++ Soal123/Soal3.java
@@ -1,4 +1,21 @@
 package Soal123;
 
+import java.util.Scanner;
+
 public class Soal3 {
+    public static void main(String[] args) {
+        int total = 1;
+        String kalimat;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Kalimat dengan camelCase : ");
+        kalimat = input.next();
+
+        char[] kalimatTerpisah = kalimat.toCharArray();
+        for (char huruf : kalimatTerpisah) {
+            if ((int)huruf >= 65 && (int)huruf <= 90) {
+                total++;
+            }
+        }
+        System.out.println(total);
+    }
 }
